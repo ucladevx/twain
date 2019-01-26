18b26f2f3434f28748048505035b775e2ddf9655
Ram Goli <ramsgoli@gmail.com>
2019-01-25 19:29:53 -0800

    insall dev dependencies

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = env => ({
+    entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        },
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          env.production ? MiniCssExtractPlugin.loader : 'style-loader',
+          { loader: 'css-loader', options: { url: false, sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } }
+        ]
+      },
+      {
+        test: /\.(jpg|png|svg|pdf)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: env.production ? '[name].[contenthash].css' : '[name].css',
+    }),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new webpack.NamedModulesPlugin()
+  ],
+  devServer: {
+    historyApiFallback: true,
+    host: '0.0.0.0',
+    port: 8080
+  }
+})
