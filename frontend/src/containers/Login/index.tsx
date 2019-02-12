7e4357f313aea141324122ade24f95a9061c668d
Justin Jeon <justinjeon151@gmail.com>
2019-02-11 20:23:49 -0800

    Fixed app; can now compile

--- frontend/src/containers/Login/index.tsx
+++ frontend/src/containers/Login/index.tsx
@@ -64,7 +64,7 @@ class Login extends React.Component<{}, State> {
           </Title>
           <Content>
             <GoogleLogin
-              clientId="" // insert client ID
+              clientId="abcdef" /* insert client ID */
               render={renderProps => (
                 <button onClick={renderProps.onClick}>
                   Login With Google (custom button incoming)
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,23 +2,12 @@ import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
-
 import FakeButton from './components/FakeButton'
 import Login from './containers/Login'
-import Home from './containers/Home'
 
+import App from './App'
 import configureStore from './configureStore'
 
-const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-      </Switch>
-    </Router>
-  )
-}
 const store = configureStore()
 
 render(
