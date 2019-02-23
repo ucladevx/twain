722d5dd4e6aa05642a4bf76ba5dbac982867e42d
Ram Goli <ramsgoli@gmail.com>
2019-02-22 19:19:33 -0800

    authentication btwn node server and postgres db successful

--- activity/server.js
+++ activity/server.js
@@ -1,8 +1,32 @@
 const express = require('express')
 const app = express()
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize('postgres', 'postgres', 'example', {
+  host: 'activity-db',
+  port: 5432,
+  dialect: 'postgres',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+})
+
 
 app.get('/', (req, res) => {
-    res.send("Hello world")
+    sequelize
+      .authenticate()
+      .then(() => {
+          res.send("Successful authentication")
+      })
+      .catch(err => {
+          console.error("Error: ", err)
+          res.send("faild to authenticate")
+      })
 })
 
 const server = app.listen(8080, () => {
