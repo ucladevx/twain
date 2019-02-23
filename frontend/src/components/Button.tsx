35d475b04b21738fddb0cf1c016b33dceb119604
Chloe Jiang <cjiang101@g.ucla.edu>
2019-02-22 19:01:09 -0800

    fixed button style parameters

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -4,18 +4,29 @@ import styled from 'styled-components'
 interface StyledButtonProps {
   primary?: boolean
   danger?: boolean
+  fillWidth?: boolean
 }
 
 const ButtonWrapper = styled.button`
   background: ${(props: StyledButtonProps) =>
     props.primary ? 'grey' : 'white'};
+  width: ${(props: StyledButtonProps) => (props.fillWidth ? '100%' : 'auto')};
   color: ${(props: StyledButtonProps) => (props.primary ? 'white' : 'grey')};
   border-radius: 5px;
-  height: 3.125 em;
-  width: auto;
+  border: solid #c4c4c4 1px;
+  height: 3.125em;
+  padding-left: 0.5em;
+  padding-right: 0.5em;
+  outline: none;
   font-family: Cabin, sans-serif;
   font-weight: bold;
   align-items: center;
+
+  &:hover {
+    cursor: pointer;
+    border: solid #c4c4c4 2px;
+    margin: -1px;
+  }
 `
 
 export default class Button extends React.Component<StyledButtonProps, {}> {
