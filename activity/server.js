7d198f2cbaef767083258a927037f8ad72da8345
ram goli <ramsgoli@gmail.com>
2019-02-22 19:21:58 -0800

    Setup Docker-compose and the activity server and postgres database (#6)
    * setup docker-compose in root dir
    
    * add activity folder to our project
    
    * setup basic node server
    
    * authentication btwn node server and postgres db successful

---
+++ activity/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize('postgres', 'postgres', 'example', {
+  host: 'activity-db',
+  port: 5432,
+  dialect: 'postgres',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+})
+
+
+app.get('/', (req, res) => {
+    sequelize
+      .authenticate()
+      .then(() => {
+          res.send("Successful authentication")
+      })
+      .catch(err => {
+          console.error("Error: ", err)
+          res.send("faild to authenticate")
+      })
+})
+
+const server = app.listen(8080, () => {
+    console.log('listening on port 8080')
+})
