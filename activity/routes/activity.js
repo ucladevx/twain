9ad9a82c1a8b646c4624e42cbbc4c42259a95f57
pranavs99 <pranavs99@github.com>
2019-02-26 15:16:41 -0800

    changed extension of route to /test, testing it rn

--- activity/routes/activity.js
+++ activity/routes/activity.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var router = express.Router();
 
-router.post("/", function (req, res) {
-    res.send("POST handler for /test route");
+router.post("/test", function (req, res) {
+    res.send("successful POST request made to /test route");
+    console.log("successful POST request made");
 });
 
+// ensuring that this code can exported to ../server.js
 module.exports = router;
--- activity/server.js
+++ activity/server.js
@@ -5,11 +5,15 @@ const app = express()
 const Sequelize = require('sequelize')
 
 // for including activity.js and its route
-const activity = require("./routes/activity");
+const activity = require("./routes/activity")
 
+// creating a new Sequelize instance with
+// database: postgres
+// username: postgres
+// password: example
 const sequelize = new Sequelize('postgres', 'postgres', 'example', {
   host: 'activity-db',
-  port: 5432,
+  port: 5432, // postgres's default port
   dialect: 'postgres',
   operatorsAliases: false,
 
@@ -21,6 +25,7 @@ const sequelize = new Sequelize('postgres', 'postgres', 'example', {
   },
 })
 
+// if we try to access 
 app.get('/', (req, res) => {
   sequelize
     .authenticate()
@@ -33,14 +38,10 @@ app.get('/', (req, res) => {
     })
 })
 
-<<<<<<< HEAD
-const server = app.listen(8000, () => {
-=======
-// telling the app that localhost:8080/activity 
+// telling the app that localhost:8080/test 
 // will use the code in the activity.js file
-app.use("/activity", activity);
+app.use("/test", activity);
 
 const server = app.listen(8080, () => {
->>>>>>> created /routes folder, testing /activity route
   console.log('listening on port 8080')
 })
