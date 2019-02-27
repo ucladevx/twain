93135d10d329850a519858c34fa2524391159da0
karenfann <kfann285@gmail.com>
2019-02-27 12:13:38 -0800

    Revert "Merge branch 'master' of https://github.com/ucladevx/DataTeam into chloe/authActions"
    This reverts commit 0b9ed4ecd0d1903b05ff084c371cd3abdf5e84ad, reversing
    changes made to 14254441e1045686276b5f54e595b99f29f46ac1.

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,103 +1,60 @@
 from flask import Flask, request, jsonify, session, redirect, url_for, escape, render_template
+from flask_oauthlib.client import OAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, login_required, login_user, logout_user, current_user
+from forms import RegForm, LoginForm 
 from mongoengine import connect
 from models import User
-from googleapiclient.discovery import build
-from oauth2client import client
-from google.oauth2 import id_token
-from google.auth.transport import requests
-
-
-import os.path
-import json
-
-
-class Response:
-    def __init__(self, code, message):
-        self.response_code = code
-        self.response_message = message 
-    
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, 
-            sort_keys=True, indent=4)
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'randomkey'
+app.config['GOOGLE_ID'] = "574982251056-km2laltubmenemsuka31uphh70km2oc0.apps.googleusercontent.com"
+app.config['GOOGLE_SECRET'] = "u-Z8wdXU3UAmc2uruNcSDySb";
 app.debug = True
+oauth = OAuth(app)
 
-connect(db = 'task-scheduler',
-    username = 'admin',
-    password = 'adminpassword',
-    host =  'mongodb://admin:adminpassword@task-scheduler-shard-00-00-x34zf.azure.mongodb.net:27017,task-scheduler-shard-00-01-x34zf.azure.mongodb.net:27017,task-scheduler-shard-00-02-x34zf.azure.mongodb.net:27017/test?ssl=true&replicaSet=task-scheduler-shard-0&authSource=admin&retryWrites=true')
+google = oauth.remote_app(
+    'google',
+    consumer_key=app.config.get('GOOGLE_ID'),
+    consumer_secret=app.config.get('GOOGLE_SECRET'),
+    request_token_params={
+        'scope': 'email'
+    },
+    base_url='https://www.googleapis.com/oauth2/v1/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+)
 
 @app.route('/')
 def index():
-    return "Base route"
-    
+    if 'google_token' in session:
+        me = google.get('userinfo')
+        return jsonify({"data": me.data})
+    return redirect(url_for('login'))
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    # Obtain authorization code and id_token from request body
-    data = request.data
-    data = json.loads(data)
-    authorization_code = data['auth_code']
-    token = data['id_token']
-
-    
-    # Retrieve client ID from client_secret file
-    if os.path.exists(os.getcwd() + '/client_secret.json'):
-        client_secret_path = os.getcwd() + '/client_secret.json'
-    
-    with open(client_secret_path, 'r') as client_secret_file:
-        client_id = json.load(client_secret_file)['web']['client_id']
-    
-    try:
-        # Verify token
-        idinfo = id_token.verify_oauth2_token(token, requests.Request(), client_id)
-
-        #If token issuer not from Google, return error
-        if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-            raise ValueError('Wrong issuer')
-
-        # ID token is valid. Query database for user email and create account if necessary
-        user = User.objects(email=idinfo['email']).first()
-        if user is None:
-            user = User(idinfo['name'].split(' ')[0], idinfo['name'].split(' ')[1], idinfo['email']).save()
-    
-        return jsonify(user)
-    except ValueError:
-        # Invalid token
-        return Reponse(301, "Error: token is invalid").toJSON()
-
-    
-
-    """
-    # Retrieve credentials from authorization code 
-    scopes = ['https://www.googleapis.com/auth/calendar']
-    credentials = client.credentials_from_clientsecrets_and_code(client_secret_path, scopes, code)
-    return jsonify(credentials)
-    """
-
-    
-    
-
-@app.route('/calendar', methods=['GET'])
-def return_calendar_data():
-
-    code = request.headers.get('code')
-    scopes = ['https://www.googleapis.com/auth/calendar']
-    credentials = client.credentials_from_clientsecrets_and_code(client_secret_path, scopes, code)
-
-    service = build('calendar', 'v3', credentials=credentials)
-
-    events = service.events().list(calendarId='primary', timeMin=now, maxResults=10)
-
-    return jsonify(events)
-
-
+    return google.authorize(callback=url_for('authorized', _external=True))
 
 @app.route('/logout')
 def logout():
-    logout_user()
-    return Reponse(200, "User logged out.").toJSON()
+    session.pop('google_token', None)
+    return redirect(url_for('index'))
+
+@app.route('/login/authorized')
+def authorized():
+    resp = google.authorized_response()
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['google_token'] = (resp['access_token'], '')
+    me = google.get('userinfo')
+    return jsonify({"data": me.data})
+
+@google.tokengetter
+def get_google_oath_token():
+    return session.get('google_token')
--- backend/app/models.py
+++ backend/app/models.py
@@ -6,8 +6,4 @@ class User(UserMixin, Document):
     first_name = StringField(max_length = 30)
     last_name = StringField(max_length = 50)
     email = StringField(max_length=30)
-
-"""
-class Credential(Document):
-    meta = {'collection': 'db-credentials'}
-"""
+    password = StringField()
