c29f6e2dc9c772e57f4c64e895bc94a7593cb8e2
Justin Jeon <justinjeon151@gmail.com>
2019-02-28 18:35:41 -0800

    Created Main page container

---
+++ frontend/src/containers/Main/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import Wrapper from '../../components/Wrapper'
+
+class Main extends React.Component<{}, {}> {
+  render() {
+    return <div>Main page goes here</div>
+  }
+}
+
+export default Main
