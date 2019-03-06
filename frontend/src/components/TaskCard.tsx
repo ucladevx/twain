3555ec457ed7cd7e1b4c3d6c4b8bf8492bc8feed
Justin Jeon <justinjeon151@gmail.com>
2019-03-06 03:09:57 -0800

    Created a layout of the fields with buttons

--- frontend/src/components/TaskCard.tsx
+++ frontend/src/components/TaskCard.tsx
@@ -3,13 +3,19 @@ import * as React from 'react'
 import styled from 'styled-components'
 import Card from './Card'
 import { Header, Subheader, Paragraph } from './Typography'
+import Button from './Button'
 
 const TaskCardWrapper = styled.div`
     padding: 0px 20px 20px 20px
     align-content: space-evenly;
     grid-column: 1 / span 4;
     grid-row: span 4
-    
+`
+
+const ButtonWrapper = styled.div`
+  grid-column: 1;
+  display: flex
+  justify-content: right;
 `
 
 export interface TaskCardProps {
@@ -29,6 +35,12 @@ export default class TaskCard extends React.Component<TaskCardProps, {}> {
           <Paragraph>{this.props.name}</Paragraph> <br />
           <Subheader>Description</Subheader>
           <Paragraph>{this.props.description}</Paragraph>
+          <Subheader>Label</Subheader>
+          <Button fillWidth primary /> <br />
+          <ButtonWrapper>
+            <Button>Cancel</Button>
+            <Button primary>Submit</Button>
+          </ButtonWrapper>
         </Card>
       </TaskCardWrapper>
     )
