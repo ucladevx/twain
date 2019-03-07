1e6d38cbef25153fb0b527e55d050d4f554c1cff
karenfann <kfann285@gmail.com>
2019-03-06 21:54:42 -0800

    component style changes

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -8,16 +8,17 @@ interface StyledButtonProps {
 
 const Button = styled.button`
   background: ${(props: StyledButtonProps) =>
-    props.primary ? 'grey' : 'white'};
+    props.primary ? 'black' : 'white'};
   width: ${(props: StyledButtonProps) => (props.fillWidth ? '100%' : 'auto')};
   color: ${(props: StyledButtonProps) => (props.primary ? 'white' : 'black')};
   border-radius: 5px;
   border: solid #c4c4c4 1px;
   height: 3.125em;
-  padding: 0 0.5em;
+  padding: 0 1em;
   outline: none;
+  font-size: 1em;
   font-family: Cabin, sans-serif;
-  font-weight: bold;
+  font-weight: 400;
   align-items: center;
 
   &:hover {
