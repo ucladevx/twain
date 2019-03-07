4f020dfabda767c44665f4f1afffbebc2ea4c479
karenfann <kfann285@gmail.com>
2019-03-06 19:29:38 -0800

    export styled components directly

--- frontend/src/components/Typography.tsx
+++ frontend/src/components/Typography.tsx
@@ -1,58 +1,22 @@
-import * as React from 'react'
 import styled from 'styled-components'
 
-const HeaderWrapper = styled.div`
+export const Header = styled.div`
   font-size: 1.5em;
   font-weight: 700;
+  font-family: Cabin;
   margin: 0.8125em 0;
 `
 
-interface HeaderProps {
-  children: React.ReactNode
-}
-
-const Header: React.SFC = (props: HeaderProps) => {
-  return (
-    <div>
-      <HeaderWrapper>{props.children}</HeaderWrapper>
-    </div>
-  )
-}
-
-const SubheaderWrapper = styled.div`
+export const Subheader = styled.div`
   font-size: 1em;
   font-weight: 700;
+  font-family: Cabin;
   margin: 0.75em 0;
 `
 
-interface SubheaderProps {
-  children: React.ReactNode
-}
-
-const Subheader: React.SFC = (props: SubheaderProps) => {
-  return (
-    <div>
-      <SubheaderWrapper>{props.children}</SubheaderWrapper>
-    </div>
-  )
-}
-
-const ParagraphWrapper = styled.div`
+export const Paragraph = styled.div`
   font-size: 1em;
   font-weight: 400;
+  font-family: Cabin;
   margin: 0.75em 0;
 `
-
-interface ParagraphProps {
-  children: React.ReactNode
-}
-
-const Paragraph: React.SFC = (props: ParagraphProps) => {
-  return (
-    <div>
-      <ParagraphWrapper>{props.children}</ParagraphWrapper>
-    </div>
-  )
-}
-
-export { Header, Subheader, Paragraph }
