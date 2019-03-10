6a8886c586d215e2e081097976cfe50578ad32ff
Chloe Jiang <cjiang101@g.ucla.edu>
2019-03-10 14:35:45 -0700

    added TimeColumn component

---
+++ frontend/src/components/TimeColumn.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const TimeColumn = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(44, 1fr);
+  height: 100%;
+`
+
+export default TimeColumn
