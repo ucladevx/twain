d15e66ddc14e3dbb7e7b343870193893f7bcdb09
pranavs99 <pranavs99@github.com>
2019-03-10 16:13:57 -0700

    fixed POST request -> SQL row pipeline

--- activity/routes/activity.js
+++ activity/routes/activity.js
@@ -2,29 +2,33 @@
 const express = require("express")
 const router = express.Router()
 const { Activity } = require("../db")
+const config = require("../Config")
 
 // the ending function for processing POST requests
 router.post("/", (request, response) => {
     const { body } = request
-    // if either the "user_id" or "activity_type" field is empty,
-    // the request is invalid
-    if (!body.user_id || !body.activity_type) {
-        response.status(400).send("invalid request: expected fields not found\n")
+
+    // if either the "user_id" or "activity_type" field is empty, the request is invalid
+    if (!body.user_uuid || !body.activity_type) {
+        return response.status(400).send("invalid request: expected fields not found\n")
     }
-    else {
-        response.status(200).send("valid request. attempting to add row to SQL table\n")
-        try {
-            // figure out how to use Sequelize to create a row in the SQL table
-            Activity.create({ uuid: "123abc", user_uuid: body.user_id, activity_type: activity_type }).then(
-                console.log("row successfully created.\n")
-            )
-        }
-        catch (error) {
-            console.log("error: ", error)
-        }
+    // confirming that the activity_type provided is an existing activity type
+    if (!config.activityTypes.includes(body.activity_type)) {
+        return response.status(400).send("invalid request: unrecognized activity_type value")
     }
-    // for debugging purposes
-    response.json(req.body)
+
+    // code for valid request
+    Activity.create({ user_uuid: body.user_uuid, activity_type: body.activity_type }).then(activity => {
+        // creating our response object if row is successfully created
+        returnObject = {
+            id: activity.get("id"),
+            activity_type: activity.get("activity_type"),
+            createdAt: activity.get("createdAt")
+        }
+        return response.json(returnObject)
+    }).catch(error => {
+        console.log("error: ", error)
+    })
 });
 
 // ensuring that this code can exported to ../server.js
