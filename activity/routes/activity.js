f23abc39f79f8343295d1da8541bb0a489cb13c8
Ram Goli <ramsgoli@gmail.com>
2019-03-11 03:31:13 -0700

    switchup response names

--- activity/routes/activity.js
+++ activity/routes/activity.js
@@ -10,33 +10,33 @@ router.post("/", (request, response) => {
 
     // if either the "user_id" or "activity_type" field is empty, the request is invalid
     if (!body.user_uuid || !body.activity_type) {
-        response_invalidRows = {
+        const response = {
             message: "invalid request: expected fields not found\n"
         }
-        return response.status(400).json(response_invalidRows)
+        return response.status(400).json(response)
     }
     // confirming that the activity_type provided is an existing activity type
     if (!config.activityTypes.includes(body.activity_type)) {
-        response_invalidActivityType = {
+        const response = {
             message: "invalid request: unrecognized activity type\n"
         }
-        return response.status(400).json(response_invalidActivityType)
+        return response.status(400).json(response)
     }
 
     // code for valid request
     Activity.create({ user_uuid: body.user_uuid, activity_type: body.activity_type }).then(activity => {
         // creating our response object if row is successfully created
-        response_success = {
+        const response = {
             id: activity.get("id"),
             activity_type: activity.get("activity_type"),
             createdAt: activity.get("createdAt")
         }
-        return response.status(200).json(response_success)
+        return response.status(200).json(response)
     }).catch(error => {
-        response_tableError = {
+        const response = {
             message: error
         }
-        return response.status(500).json(response_tableError)
+        return response.status(500).json(response)
     })
 });
 
