c32960f9d53785d07256311e03b6cb556496ed69
Ram Goli <ramsgoli@gmail.com>
2019-03-11 03:42:13 -0700

    fix(): change req and res method names

--- activity/routes/activity.js
+++ activity/routes/activity.js
@@ -4,23 +4,22 @@ const router = express.Router()
 const { Activity } = require("../db")
 const config = require("../Config")
 
-// the ending function for processing POST requests
-router.post("/", (request, response) => {
-    const { body } = request
+router.post("/", (req, res) => {
+    const { body } = req
 
     // if either the "user_id" or "activity_type" field is empty, the request is invalid
     if (!body.user_uuid || !body.activity_type) {
         const response = {
-            message: "invalid request: expected fields not found\n"
+            message: "invalid request: expected fields not found"
         }
-        return response.status(400).json(response)
+        return res.status(400).json(response)
     }
     // confirming that the activity_type provided is an existing activity type
     if (!config.activityTypes.includes(body.activity_type)) {
         const response = {
-            message: "invalid request: unrecognized activity type\n"
+            message: "invalid request: unrecognized activity type"
         }
-        return response.status(400).json(response)
+        return res.status(400).json(response)
     }
 
     // code for valid request
@@ -31,12 +30,12 @@ router.post("/", (request, response) => {
             activity_type: activity.get("activity_type"),
             createdAt: activity.get("createdAt")
         }
-        return response.status(200).json(response)
+        return res.status(200).json(response)
     }).catch(error => {
         const response = {
             message: error
         }
-        return response.status(500).json(response)
+        return res.status(500).json(response)
     })
 });
 
