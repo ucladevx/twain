77b900e83e66fa565bd35bdbe6fd8c78689f255b
Zach Qian <qianzach@gmail.com>
2019-05-10 13:15:26 -0700

    set up request and response objects

--- activity/tests/routes/activity.test.js
+++ activity/tests/routes/activity.test.js
@@ -1,1 +1,15 @@
 // tests go here
+const expect = require('chai').expect;
+const { activityTest } = require('../../routes/activity.js');
+
+// create an empty request object with an empty body
+let request = {
+    body: {},
+};
+
+let response = {
+    sendCalledWith: '',
+    send: function (arg) {
+        this.sendCalledWith = arg;
+    }
+};
