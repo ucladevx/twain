d7b186abe7de9454396dbd7505134f519c2ae606
pranavs99 <pranavs99@github.com>
2019-05-15 21:08:30 -0700

    fixed read functionality with error checking using .count()

--- activity/routes/activity.js
+++ activity/routes/activity.js
@@ -24,43 +24,67 @@ router.post("/", (req, res) => {
     }
 
     // code for valid request
-    Activity.create({ user_uuid: body.user_uuid, activity_type: body.activity_type }).then(activity => {
-        // creating our response object if row is successfully created
-        const response = {
-            id: activity.get("id"),
-            activity_type: activity.get("activity_type"),
-            createdAt: activity.get("createdAt")
-        }
-        return res.status(200).json(response)
-    }).catch(error => {
-        const response = {
-            message: error
-        }
-        return res.status(500).json(response)
-    })
+    Activity
+        .create({ user_uuid: body.user_uuid, activity_type: body.activity_type }).then(activity => {
+            // creating our response object if row is successfully created
+            const response = {
+                id: activity.get("id"),
+                activity_type: activity.get("activity_type"),
+                createdAt: activity.get("createdAt")
+            }
+            return res.status(200).json(response)
+        }).catch(error => {
+            const response = {
+                message: error
+            }
+            return res.status(500).json(response)
+        })
 })
 
 // if you perform a GET request, we'll read from the table instead
 // uuid corresponds to the user's user_uuid for which we want data
 router.get("/:requested_user", (req, res) => {
-    // at first, just try reading every single element 
-    Activity.findAll({
-        // looking specifically for the user_uuid that matches :requested_user
-        where: {
-            user_uuid: req.params.requested_user
-        }
-    }).then(userActivity => {
-        // if findAll() is successful, then the return object is called
-        // userActivity and JSONified using .json()
-        return res.status(200).json(userActivity)
-    }).catch(error => {
-        // send the error back as a JSON with field "message"
-        const response = {
-            message: error,
-            text: "error received"
-        }
-        return res.status(500).json(response)
-    })
+    // first, let's perform a .count() operation on the table
+    // if that returns an error, we know the entry doesn't exist in
+    // the table
+    Activity
+        .count({
+            where: {
+                user_uuid: req.params.requested_user
+            }
+        })
+        // if .count() is successful, we query for the requested user_uuid
+        .then(n_occurrences => {
+            Activity
+                .findAll({
+                    where: {
+                        user_uuid: req.params.requested_user
+                    }
+                })
+                // if .findAll() is successful, return the result of the database
+                // query in the returned JSON object
+                .then(result => {
+                    const response = {
+                        count: n_occurrences,
+                        entries: result
+                    }
+                    return res.status(200).json(response)
+                })
+                // if .findAll() is unsuccessful, return a status code 500
+                // INTERNAL SERVER ERROR along with Sequelize's error message
+                .catch(error => {
+                    const response = {
+                        message: error
+                    }
+                    return res.status(500).json(response)
+                })
+        })
+        .catch(error => {
+            const response = {
+                message: "invalid request: user_uuid '" + req.params.requested_user + "' not found"
+            }
+            return res.status(400).json(response)
+        })
 })
 
 // ensuring that this code can exported to ../server.js
