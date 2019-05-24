4ed63eef8c06f2a14a2cc43318ebaa20b830a290
pranavs99 <pranavs99@github.com>
2019-05-23 19:26:43 -0700

    team meeting commit 5/23 7:26pm

--- activity/routes/milestones.js
+++ activity/routes/milestones.js
@@ -4,6 +4,13 @@ const router = express.Router()
 const { Activity } = require("../db")
 const config = require("../Config")
 
+// POST request handler
+router.post("/", (req, res) => {
+    const { body } = req
+
+    // if either 
+})
+
 // testing GET request handler
 router.get("/", (req, res) => {
     response = {
