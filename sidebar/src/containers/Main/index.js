feb3b21ef521f711e12c2b9736d8669b65cfd4ec
Rajiv Anisetti <rajiv.anisetti@gmail.com>
2019-06-06 16:16:55 -0700

    Not working, but has function skeletons

--- sidebar/src/containers/Main/index.js
+++ sidebar/src/containers/Main/index.js
@@ -2,8 +2,15 @@ import * as React from "react";
 import styled from "styled-components";
 import Wrapper from "../../components/Wrapper";
 import TaskList from "../../components/TaskList";
+const getFreeIntervals = require('../../lib/storage/free_intervals')
+//import { schedule } from '../../lib/csp/schedule'
 
 class Main extends React.Component {
+  async componentDidMount() {
+    var res = await getFreeIntervals();
+    console.log(res);
+  }
+
   render() {
     return (
       <div className="Main">
---
+++ sidebar/src/lib/storage/free_intervals.js
@@ -0,0 +1,108 @@
+/*global chrome*/
+var API_KEY = 'AIzaSyAx2zbBHSisuacAEQDaXB7GbnK4VIvITrM'
+function getTodaysEvents() {
+    chrome.identity.getAuthToken({ 'interactive': true }, function (token) {
+        var init = {
+            'method': 'GET',
+            'async': true,
+            'headers': {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json'
+            },
+            'contentType': 'json'
+        };
+
+        var time_now = new Date()
+        var time_midnight = new Date()
+        time_midnight.setDate(time_midnight.getDate() + 1)
+        time_midnight.setHours(0)
+        time_midnight.setMinutes(0)
+        time_midnight.setSeconds(0)
+        time_midnight.setMilliseconds(0)
+
+        var time_now_string = ISODateString(time_now)
+        var time_midnight_string = ISODateString(time_midnight)
+
+        var url = 'https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMax=' + time_midnight_string + '&timeMin=' + time_now_string + '&orderBy=startTime&singleEvents=true' + '&key=' + API_KEY
+
+
+        fetch(url, init)
+            .then((response) => response.json()) // Transform the data into json
+            .then(function (data) {
+                return data['items'];
+            })
+    })
+}
+
+function getHourDifference(start, end) {
+
+    return end.getUTCHours() - start.getUTCHours() + (end.getUTCMinutes() - start.getUTCMinutes()) / 60;
+
+}
+
+function Interval(start, end, id) {
+    this.start = start;
+    this.end = end;
+    this.duration = getHourDifference(start, end);
+    this.intervalId = id;
+}
+
+function getTimeMidnight() {
+    var time_midnight = new Date()
+    time_midnight.setDate(time_midnight.getDate() + 1)
+    time_midnight.setHours(0)
+    time_midnight.setMinutes(0)
+    time_midnight.setSeconds(0)
+    time_midnight.setMilliseconds(0)
+    return time_midnight
+}
+
+async function getFreeIntervals() {
+
+    var events = await getTodaysEvents()
+    console.log("Getting intervals")
+    console.log(events)
+
+    var time_now = new Date()
+    var time_midnight = getTimeMidnight()
+
+    var free_interval_list = new Array()
+    var i = 2
+    free_interval_list.push(new Interval(time_now, time_midnight, 1))
+
+    for (var i = 0; i < events.length; i++) {
+
+        var curr_event = events[i];
+
+        var latestFreeInterval = free_interval_list[free_interval_list.length() - 1];
+        var event_start = new Date(curr_event['start']['dateTime']);
+        var event_end = new Date(curr_event['end']['dateTime']);
+
+        if (event_start > latestFreeInterval.start) {
+            var new_interval = new Interval(event_end, latestFreeInterval.end, i)
+            latestFreeInterval.end = new Date(event_start);
+            latestFreeInterval.duration = getHourDifference(latestFreeInterval.start, latestFreeInterval.end)
+            free_interval_list.push(new_interval)
+            i++;
+        }
+        else if (event_end > latestFreeInterval.start) {
+            latestFreeInterval.start = event_end
+        }
+
+
+    }
+
+    return free_interval_list;
+}
+
+function ISODateString(d) {
+    function pad(n) { return n < 10 ? '0' + n : n }
+    return d.getUTCFullYear() + '-'
+        + pad(d.getUTCMonth() + 1) + '-'
+        + pad(d.getUTCDate()) + 'T'
+        + pad(d.getUTCHours()) + ':'
+        + pad(d.getUTCMinutes()) + ':'
+        + pad(d.getUTCSeconds()) + 'Z'
+}
+
+module.exports = { getFreeIntervals }
