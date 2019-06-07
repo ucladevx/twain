8ff6f4702f30970912b6c9b678d7cc385bd89db2
karenfann <kfann285@gmail.com>
2019-06-06 20:07:35 -0700

    almost fucking there

---
+++ sidebar/src/constants/views.js
@@ -0,0 +1,8 @@
+const views = {
+    List: 'List',
+    NewTask: 'NewTask',
+    EditTask: 'EditTask',
+    Confirmation: 'Confirmation',
+  }
+
+export views
--- sidebar/src/containers/Main/index.js
+++ sidebar/src/containers/Main/index.js
@@ -2,13 +2,12 @@ import * as React from "react";
 import styled from "styled-components";
 import Wrapper from "../../components/Wrapper";
 import TaskList from "../../components/TaskList";
-import { getTodaysFreeIntervals } from '../../lib/storage/free_intervals'
+import getTodaysFreeIntervals from "../../lib/storage/free_intervals";
 //import { schedule } from '../../lib/csp/schedule'
 
 class Main extends React.Component {
   componentDidMount() {
-    // try {
-    var free_intervals = getTodaysFreeIntervals();
+    console.log(getTodaysFreeIntervals());
     // console.log(free_intervals);
     // }
     // catch (err) {
